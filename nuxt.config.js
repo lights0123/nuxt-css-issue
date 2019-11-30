72275b718b7eaa0bf5fdec0d8b50a109d39a47dc
lights0123 <developer@lights0123.com>
2019-11-30 11:44:30 -0500

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+const config = {
+
+	/*
+  ** Headers of the page
+  */
+	head: {
+		title: 'Title',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+		],
+	},
+
+	/*
+  ** Customize the progress-bar color
+  */
+	loading: { color: '#fff' },
+
+	/*
+  ** Global CSS
+  */
+	css: [
+	],
+
+	/*
+  ** Plugins to load before mounting the App
+  */
+	/*
+  ** Nuxt.js modules
+  */
+
+	/*
+  ** Build configuration
+  */
+	build: {
+		extend(config, ctx) {
+
+		}
+	}
+};
+export default config;
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import '~/assets/test.css';
+// import 'element-ui/packages/theme-chalk/src/button.scss';
+
+export default {
+};
+</script>
